d525ebee9f3f962ea4bb57fce57486d8770f5c42
Md Arifur Rahman <arif.aust.eng@gmail.com>
2023-10-31 10:38:21 +0100

    registraion form done

--- registration.php
+++ registration.php
@@ -1,31 +1,37 @@
 <?php
-session_start([
-    "name" => "registration",
-]);
+session_start();
 
 $usersFile = "users.json";
-$users = file_exists($usersFile) ? json_decode(file_get_contents($usersFile, true)) : [];
+$users = file_exists($usersFile) ? json_decode(file_get_contents($usersFile), true) : [];
+
+
 // print_r($users);
 
+function saveUsers($file, $users)
+{
+    file_put_contents($file, json_encode($users, JSON_PRETTY_PRINT));
+}
+
 if (isset($_POST["registration"])) {
-    $name = $_POST["name"];
-    $name = $_POST["email"];
-    $name = $_POST["password"];
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
 
     // validation logic
-    if (empty($name) || empty($email) || empty($password)) {
-        $errorMsg = "Please fill all the input fields";
+    if (empty($username) || empty($email) || empty($password)) {
+    //    echo "Please fill all the input fields";
+       
     } else {
         if (isset($users[$email])) {
-            echo "This email already used";
+            $errorMsg = "This email already used";
         } else {
             $users[$email] = [
-                "name" => $name,
+                "username" => $username,
                 "password" => $password,
                 "role" => ""
             ];
-
-            $saveUsers ($users, $usersFile );
+            // print_r($users);
+            saveUsers($usersFile, $users);
         }
     }
 }
@@ -36,14 +42,14 @@ if (isset($_POST["registration"])) {
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta username="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration Form</title>
 </head>
 
 <body>
     <form action="" method="POST">
         <label for="">Enter Name: </label> <br>
-        <input type="text" name="name" id=""> <br> <br>
+        <input type="text" name="username" id=""> <br> <br>
         <label for="">Enter Email: </label> <br>
         <input type="email" name="email" id=""> <br> <br>
         <label for="">Enter Password: </label> <br>
