a1be313f540028e0da2bb803802ba41a1a55cb3d
Md Arifur Rahman <arif.aust.eng@gmail.com>
2023-10-31 10:02:09 +0100

    validation for registration done

--- registration.php
+++ registration.php
@@ -1,9 +1,36 @@
+<?php
+session_start([
+    "name" => "registration",
+]);
+
+$usersFile = "users.json";
+$users = file_exists($usersFile) ? json_decode(file_get_contents($usersFile, true)) : [];
+ print_r ($users);
+
+ if (isset($_POST["registration"])){
+    $name = $_POST["name"];
+    $name = $_POST["email"];
+    $name = $_POST["password"];
+
+    // validation logic
+    if (empty($name) || empty($email) || empty($password)){
+
+    if (isset($users["email"])){
+        echo "This email already used";
+    } else {
+       
+    }
+
+        }
+ }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Registration Form</title>
 </head>
 <body>
     <form action="" method="POST">
