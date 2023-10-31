c85b5904d1e4b6982fefd709d4499fbcd0082c5c
Md Arifur Rahman <arif.aust.eng@gmail.com>
2023-10-31 14:21:57 +0100

    admin page done

--- login.php
+++ login.php
@@ -4,61 +4,58 @@ session_start();
 $usersFile = "users.json";
 $users = file_exists($usersFile) ? json_decode(file_get_contents($usersFile), true) : [];
 
-echo $_SESSION["email"];
+if (isset($_SESSION["email"])) {
+    // User is already logged in, handle as needed
+    $userRole = $users[$_SESSION["email"]]["role"];
+    if ($userRole === "manager") {
+        header("Location: manager.php");
+        exit;
+    } elseif ($userRole === "admin") {
+        header("Location: role_management.php");
+        exit;
+    } else {
+        header("Location: user.php");
+        exit;
+    }
+}
 
- if (isset($_POST["login"])) {
+if (isset($_POST["login"])) {
+    $login_email = $_POST["email"];
+    $login_password = $_POST["password"];
 
-     $login_email = $_POST["email"];
-     $login_password = $_POST["password"];
-     echo ($login_email); 
-
-     if ($_SESSION["email"] === $login_email && $login_password === $users[$login_email]["password"] ){
-        foreach ($users as $user) {
-            if ($user["role"] === "manager") {
-                header("Location: manager.php");
-            } else if($user["role"] === "admin") {
-                header("Location: role_management.php");
-            } else {
-                header("Location: user.php");
-            }
-        }
-        echo "user found";
-     } else {
-        echo "user not found";
-     }
-    
-    // foreach ($users as $user) {
-
-    //     if ()
-    // }
-
-
-
-//         // if ($user["role"] === "x") {
-//         //     header("Location: user.php");
-//         // } 
+    if (isset($users[$login_email]) && $login_password === $users[$login_email]["password"]) {
+        $_SESSION["email"] = $login_email;
+        $userRole = $users[$login_email]["role"];
         
-//    }
- }
-
+        if ($userRole === "manager") {
+            header("Location: manager.php");
+            exit;
+        } elseif ($userRole === "admin") {
+            header("Location: role_management.php");
+            exit;
+        } else {
+            header("Location: user.php");
+            exit;
+        }
+    } else {
+        echo "User not found or password is incorrect.";
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
 </head>
-
 <body>
     <form action="" method="POST">
-        <label for="">Enter Email:</label> <br>
-        <input type="text" name="email"> <br> <br>
-        <label for="">Enter Password:</label> <br>
-        <input type="text" name="password"> <br> <br>
+        <label for="email">Enter Email:</label><br>
+        <input type="text" name="email"><br><br>
+        <label for="password">Enter Password:</label><br>
+        <input type="password" name="password"><br><br>
         <button type="submit" name="login">Login</button>
     </form>
 </body>
-
 </html>
--- role_management.php
+++ role_management.php
@@ -1,3 +1,34 @@
+<?php
+session_start();
+
+$usersFile = "users.json";
+$users = file_exists($usersFile) ? json_decode(file_get_contents($usersFile), true) : [];
+
+if (isset($_POST["edit_role"])) {
+    $selectedUser = $_POST["select_user"];
+    $newRole = $_POST["select_role"];
+
+    if (isset($users[$selectedUser])) {
+        // Update the user's role
+        $users[$selectedUser]["role"] = $newRole;
+        // Save the updated user data back to the JSON file
+        file_put_contents($usersFile, json_encode($users, JSON_PRETTY_PRINT));
+        header("Location: role_management.php");
+        exit;
+    }
+} elseif (isset($_POST["delete_user"])) {
+    $selectedUser = $_POST["select_user"];
+
+    if (isset($users[$selectedUser])) {
+        // Remove the user from the user data
+        unset($users[$selectedUser]);
+        // Save the updated user data back to the JSON file
+        file_put_contents($usersFile, json_encode($users, JSON_PRETTY_PRINT));
+        header("Location: role_management.php");
+        exit;
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,16 +37,24 @@
     <title>Role Management</title>
 </head>
 <body>
-
-        <form action="">
-           <label for="">Edit Role: </label> <br>
-            <select name="select_role" id="">
+    <form action="role_management.php" method="post">
+        <label for="select_user">Select User:</label><br>
+        <select name="select_user" id="select_user">
+            <?php
+            // Populate the dropdown with user emails from your user data
+            foreach ($users as $email => $userData) {
+                echo '<option value="' . $email . '">' . $email . '</option>';
+            }
+            ?>
+        </select><br><br>
+        <label for="select_role">Edit Role:</label><br>
+        <select name="select_role" id="select_role">
             <option value="admin">Admin</option>
             <option value="manager">Manager</option>
             <option value="user">User</option>
-            </select> <br> <br>
-            <button type="submit"> Delete</button>
-        </form>
-    
+        </select><br><br>
+        <button type="submit" name="edit_role">Edit Role</button>
+        <button type="submit" name="delete_user">Delete User</button>
+    </form>
 </body>
 </html>
